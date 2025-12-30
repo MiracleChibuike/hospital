a7bd4c82aeab97eb6c615abc7b1e317ef3eefa3e
TChidiDev <tchididev2gmail.com>
2025-12-30 22:01:44 +0100

    Restructured the file system v2

--- src/Admin.php
+++ src/Admin.php
@@ -2,27 +2,71 @@
 require_once 'User.php';
 class Admin extends User
 {
-  private $adminLevel;
 
-  public function __construct($username, $email, $adminLevel)
+
+  private $adminRole;
+  // private $adminId;
+
+  public function __construct()
   {
-    parent::__construct($username, $email);
-    $this->adminLevel = $adminLevel;
+    parent::__construct();
+    $this->setTable('admin');
   }
 
-  public function getAdminLevel()
+  public function getAdminRole($adminId): bool | string
   {
-    return $this->adminLevel;
+    $sql = "SELECT admin_role FROM " . $this->table . " WHERE admin_id = :admin_id";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(":admin_id", $adminId, PDO::PARAM_STR);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
   }
 
-  public function setAdminLevel($adminLevel)
+  public function setAdminRole($adminId, $adminRole)
   {
-    $this->adminLevel = $adminLevel;
+    $sql = "UPDATE " . $this->table . " SET admin_role = :admin_role WHERE admin_id = :admin_id";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(":admin_id", $adminId, PDO::PARAM_STR);
+    $stmt->bindValue(":admin_role", $adminRole, PDO::PARAM_STR);
+    return $stmt->execute();
   }
 
-  public function displayInfo()
+  public function register(array $adminData, $role): bool
   {
-    parent::displayInfo();
-    echo "Admin Level: " . $this->adminLevel . "\n";
+    if ($role !== 'superadmin') {
+      // throw new Exception("Only superadmins can create new admins.");
+      return "Only superadmins can create new admins.";
+    } else {
+
+      $sql = "INSERT INTO " . $this->table . " (firstname, lastname, email, password, username, gender, admin_role) VALUES (:firstname, :lastname, :email, :password, :username, :gender, :admin_role)";
+
+      $stmt = $this->conn->prepare($sql);
+
+      $stmt->bindValue(":firstname", $adminData['firstname'], PDO::PARAM_STR);
+      $stmt->bindValue(":lastname", $adminData['lastname'], PDO::PARAM_STR);
+      $stmt->bindValue(":email", $adminData['email'], PDO::PARAM_STR);
+      $stmt->bindValue(":password", password_hash($adminData['password'], PASSWORD_DEFAULT), PDO::PARAM_STR);
+      $stmt->bindValue(":username", $adminData['username'], PDO::PARAM_STR);
+      $stmt->bindValue(":gender", $adminData['gender'], PDO::PARAM_STR);
+      $stmt->bindValue(":admin_role", $adminData['admin_role'], PDO::PARAM_STR);
+
+      return $stmt->execute();
+    }
+  }
+
+
+
+
+
+  public function deleteAdmin($adminId, $role): bool
+  {
+    if ($role !== 'superadmin') {
+      throw new Exception("Only superadmins can delete admins.");
+    } else {
+      $sql = "DELETE FROM " . $this->table . " WHERE admin_id = :admin_id";
+      $stmt = $this->conn->prepare($sql);
+      $stmt->bindValue(":admin_id", $adminId, PDO::PARAM_STR);
+      return $stmt->execute();
+    }
   }
 }
