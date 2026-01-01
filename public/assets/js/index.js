02b1151e4b1226ab14863e5536eac7c2ab3073d1
MiracleChibuike <miraclechibuike27@gmail.com>
2026-01-01 02:51:12 +0100

    add back the loader --not important

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,28 +3,25 @@
 
 // Loads the animation on a succesful page load
 
-// const AnimationLoad = () => {
-//   const loader = document.querySelector(".loader");
-//   var headre = document.getElementById("header");
-//   var mainDiv = document.querySelector(".main");
+const AnimationLoad = () => {
+  const loader = document.querySelector(".loader");
+  var headre = document.getElementById("header");
+  var mainDiv = document.querySelector(".main");
 
-//   // Show Loader Initially
-//   headre.style.display = "block";
+  // Show Loader Initially
+  headre.style.display = "block";
 
-//   // After 6 seconds, hide the headre and show the mainDiv
-//   setTimeout(() => {
-//     if (mainDiv.style.display = "none") {
-//         mainDiv.style.display = "block";
-//         headre.style.display = "none";
-//     }
-//   }, 12000);
-// };
+  // Hide loader according to when Page is ready
+  if ((mainDiv.style.display = "none")) {
+    headre.style.display = "none";
+    mainDiv.style.display = "block"
+  }
+};
 
-// // Call the AnimationLoad function when page is ready
-// AnimationLoad();
+// Call the AnimationLoad function when page is ready
+document.addEventListener("DOMContentLoaded", AnimationLoad);
 
 // Prevent Image dragging
-
 document.querySelectorAll("img").forEach((img) => {
   img.addEventListener("mousedown", function (event) {
     event.preventDefault();
